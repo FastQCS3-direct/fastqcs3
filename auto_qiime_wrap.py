d3bdf74cac825a06a95706e7787734e83c7b5e99
Kaylyn Torkelson <katorke@gmail.com>
2021-03-15 13:30:33 -0700

    Removing old files

--- auto_qiime_wrap.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def auto_qiime(directory,trimlength):
-    subprocess.run(['bash','-c','bash auto_qiime.sh '+directory+' '+trimlength])
-
-directory=input('Directory of .fastq files:')
-trimlength=input('Sequencing trim length:')
-
-auto_qiime(directory,trimlength)
