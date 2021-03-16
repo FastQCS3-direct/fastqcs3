7ff8caca3f61777a7377aaf3825d46fa9bf03889
Cassandra Maranas <cjm5033@uw.edu>
2021-03-16 10:17:59 -0700

    deleted commented out function

--- py_files/data_prep_stack_barplots.py
+++ py_files/data_prep_stack_barplots.py
@@ -32,30 +32,12 @@ def make_tax_dfs(taxon_list, feature_table):
     # columns are categories within each taxonomy level found in data, indexes are the samples
     return kingdom_df, phylum_df, class_df, order_df, family_df, genus_df, species_df
 
-# def fill_tax_dfs(kingdom_df, phylum_df, class_df, order_df, family_df, genus_df, species_df, feature_table, taxonomy_red):
-#     """fills dataframes for each taxonomy level for creating stacked bar graph"""
-#     for sample, row in feature_table.iterrows():
-#         # iterate over feature table rows 
-#         for featid in row.index:
-#             # iterate over each feature id (sequence) in a sample and add appropriate prevalence to each taxonomy df
-            
-#             tax_list = taxonomy_red.loc[featid] # gets a list of the corresponding kingdom, phylum, etc for the feature id
-#             kingdom_df[tax_list[0]][sample] += feature_table.loc[sample][featid] / 100
-#             phylum_df.loc[sample][tax_list[1]] += feature_table.loc[sample][featid] / 100
-#             class_df.loc[sample][tax_list[2]] += feature_table.loc[sample][featid] / 100
-#             order_df.loc[sample][tax_list[3]] += feature_table.loc[sample][featid] / 100
-#             family_df.loc[sample][tax_list[4]] += feature_table.loc[sample][featid] / 100
-#             genus_df.loc[sample][tax_list[5]] += feature_table.loc[sample][featid] / 100
-#             species_df.loc[sample][tax_list[6]] += feature_table.loc[sample][featid] / 100
-#     # return the filled dataframes for each taxonomy level
-#     return kingdom_df, phylum_df, class_df, order_df, family_df, genus_df, species_df
-
 def fill_tax_dfs(kingdom_df,phylum_df,class_df,order_df,family_df,genus_df,species_df,feature_table,taxonomy_red):
     """fills dataframes for each taxonomy level for creating stacked bar graph"""
     for sample, row in feature_table.iterrows(): 
         # iterate over feature rows
         for featid in row.index:
-            # iterate over each feature id (sequence) insample and add prevalence to taxonomy df
+            # iterate over each feature id (sequence) in sample and add prevalence to taxonomy df
             if featid in taxonomy_red.index:
                 tax_list = taxonomy_red.loc[featid]
                 # gets a list of the corresponding kingdom, phylum, etc for the feature id
@@ -76,6 +58,8 @@ def prepare_data_stacked_barplots(feature_table, taxonomy_table):
     taxonomy_red = reduce_taxonomy_df(taxonomy_table)
     taxon_list = make_taxon_list(taxonomy_red)
     kingdom_df_0, phylum_df_0, class_df_0, order_df_0, family_df_0, genus_df_0, species_df_0 = make_tax_dfs(taxon_list, feature_table)
-    kingdom_df, phylum_df, class_df, order_df, family_df, genus_df, species_df = fill_tax_dfs(kingdom_df_0, phylum_df_0, class_df_0, order_df_0, 
-                                                                                              family_df_0, genus_df_0, species_df_0, feature_table, taxonomy_red)
+    kingdom_df, phylum_df, class_df, order_df, family_df, genus_df, species_df = fill_tax_dfs(kingdom_df_0, phylum_df_0, 
+                                                                                              class_df_0, order_df_0, 
+                                                                                              family_df_0, genus_df_0, 
+                                                                                              species_df_0, feature_table, taxonomy_red)
     return kingdom_df, phylum_df, class_df, order_df, family_df, genus_df, species_df
